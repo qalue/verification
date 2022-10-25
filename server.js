4beae1e0b8f6407973ad81aa4fe4b5cc0be9a708
adam <55650376+qalue@users.noreply.github.com>
2022-10-25 21:08:10 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+import fastify from 'fastify';
+import rawBody from 'fastify-raw-body';
+import { InteractionResponseType, InteractionType, verifyKey } from "discord-interactions";
+
+const server=fastify({
+    logger: true
+})
+
+server.register(rawBody,{
+    runFirst: true
+})
+
+server.get('/',(request,response)=>{
+    server.log.info('Handling GET request')
+})
+
+server.addHook('preHandler',async(request,response)=>{
+    if (request.method === 'POST') {
+        const signature = request.headers['x-signature-ed25519'];
+        const timestamp = request.headers['x-signature-timestamp'];
+        const isValidRequest = verifyKey(
+            request.rawBody,
+            signature,
+            timestamp,
+            process.env.PUBLIC_KEY
+        );
+        if (!isValidRequest) {
+            server.log.info('Invalid Request');
+            return response.status(401).send({ error: 'Bad request signature ' });
+        }
+    }
+})
+
+server.post("/", async (request, response) => {
+    const message=request.body;
+
+    if(message.type===InteractionType.PING) {
+        server.log.info("Handling Ping request");
+        response.send({
+        type: InteractionResponseType.PONG,
+        });
+    }else{
+        server.log.error("Unknown Type");
+        response.status(400).send({ error: "Unknown Type" });
+    }
+});
+
+server.listen(3000,async(error,address)=>{
+    if(error){
+        server.log.error(error)
+        process.exit(1)
+    }
+    server.log.info(`Server listening on ${address}`)
+})
